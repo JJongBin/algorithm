65a0a7fd66868f9a920eaed7edfd2e8b81385cc6
원종빈의 MacBook <gghkdu2@gmail.com>
2021-07-13 12:31:07 +0900

    기본수학1 완료

--- 8_기본수학1/9_1011.py
+++ 8_기본수학1/9_1011.py
@@ -3,21 +3,24 @@ n = int(input())
 targets = []
 for i in range(n):
     target = list(map(int, input().split()))
-    targets.append()
+    targets.append(target)
 
-for target in targets:
-    prev_move = 0
-    cnt = 0
-    length = target[1] - target[0] -1
-    
-    while prev_move > 0:
-        if prev_move == 0:
-            length -= 1 
-            cnt += 1
-        elif length > (prev_move + 1):
-            length -= (prev_move + 1)
-            cnt += 1
+for i in targets:
+    temp = 1
+    target = i[1] - i[0]
 
-        elif length > prev_move
-        # 잘못생각했어!
-# 무조건 마지막은 1이니까 그전은 2보다 작아야됨 / 그 전전은 3보다 작아야됨 
+    while True:
+        if (temp ** 2) <= target and ((temp + 1) ** 2) > target:
+            break
+        temp += 1
+
+    if (temp ** 2) == target:
+        print((temp * 2) - 1)
+
+    elif (temp ** 2) < target <= (temp ** 2) + temp:
+        print((temp * 2))
+
+    else:
+        print((temp * 2) + 1)
+
+## 먼저 결과값을 나열해 규칙을 찾아본다!!
