687d610cd7d20ff54ddb918a4f11a6a01b6295a5
원종빈의 MacBook <gghkdu2@gmail.com>
2021-08-06 14:58:02 +0900

    14 백트래킹 - 3번까지

---
+++ 14_백트래킹/3_15651.py
@@ -0,0 +1,18 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+num = m
+result = []
+def f(check, m, n):
+    if m == 0:
+        print(" ".join(map(str, result)))
+        return
+    else:
+        for i in range(1, n+1):
+            result.append(i)
+            f(check, m-1, n)
+            result.pop()
+            
+
+f(0, m, n)
+
