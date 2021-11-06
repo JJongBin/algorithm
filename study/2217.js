1bfc9c3b7eeedf99af5f686f4c6a7e7d62d7c04d
JJongBin <gghkdu2@gmail.com>
2021-11-06 22:43:48 +0900

    docs: Update BOJ problem

--- study/2217.js
+++ study/2217.js
@@ -10,6 +10,7 @@ arr.sort((a, b) => b - a);
 
 let min = 0;
 let answer = 0;
+// 현재까지의 가장 적은 밧줄에 곱하기 밧줄의 개수
 for(let i = 0; i < n; i++){
     min = arr[i];
     answer = Math.max(answer, min*(i+1));
