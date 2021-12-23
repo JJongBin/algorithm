b32eb9083cee8e1dbe45fa502ea783644d9fef06
JJongBin <gghkdu2@gmail.com>
2021-12-23 19:06:14 +0900

    docs: fixed Leetcode 516 problem

--- study/leetcode516.js
+++ study/leetcode516.js
@@ -1,24 +1,21 @@
-var countSubstrings = function (s) {
+var longestPalindromeSubseq = function (s) {
+  let answer = 0;
   const len = s.length;
-  let answer = len;
   const dp = new Array(len);
   for (let i = 0; i < len; i++) dp[i] = new Array(len).fill(0);
 
   for (let i = 0; i < len; i++) dp[i][i] = 1;
   for (let i = 0; i < len - 1; i++) {
-    if (s[i] === s[i + 1]) {
-      answer++;
-      dp[i][i + 1] = 1;
-    }
+    if (s[i] === s[i + 1]) dp[i][i + 1] = 2;
+    else dp[i][i + 1] = 1;
   }
+
   for (let i = len - 3; i >= 0; i--) {
     for (let j = i + 2; j < len; j++) {
-      if (s[i] === s[j] && dp[i + 1][j - 1] === 1) {
-        answer++;
-        dp[i][j] = 1;
-      }
+      if (s[i] === s[j]) dp[i][j] = dp[i + 1][j - 1] + 2;
+      else dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
     }
   }
+  answer = dp[0][len - 1];
   return answer;
 };
-console.log(countSubstrings('aaa'));
