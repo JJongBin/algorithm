8db1f3c26518c6fbdb6875a3a85873cd6c655d48
JJongBin <gghkdu2@gmail.com>
2022-08-08 23:34:20 +0900

    Solve beakjoon 10610 problem

--- study/10610.js
+++ study/10610.js
@@ -3,10 +3,11 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './testcase/10610
 
 let input = fs.readFileSync(filePath).toString().trim();
 
-console.log(
-  input
-    .split('')
-    .map(Number)
-    .sort((a, b) => b - a)
-    .join('')
-);
+const nums = input
+  .split('')
+  .map(Number)
+  .sort((a, b) => b - a);
+
+const sum = nums.reduce((a, b) => a + b, 0);
+if (sum % 3 !== 0 || nums[nums.length - 1] !== 0) console.log(-1);
+else console.log(nums.join(''));
