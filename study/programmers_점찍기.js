fa2b828d5db04904d93b9e1802250f91a0f34153
JJongBin <gghkdu2@gmail.com>
2023-02-16 14:05:52 +0900

    solve programmers problem

---
+++ study/programmers_점찍기.js
@@ -0,0 +1,10 @@
+function solution(k, d) {
+  var answer = 0;
+
+  for (let i = 0; i <= d; i += k) {
+    answer += Math.floor(Math.sqrt(d ** 2 - i ** 2) / k) + 1;
+  }
+  return answer;
+}
+
+console.log(solution(2, 4));
