788998a030b35492a6d0f4a51dc3784ce176c331
JJongBin <gghkdu2@gmail.com>
2023-03-26 13:55:20 +0900

    fix programmers problem

--- study/programmers_광물캐기.js
+++ study/programmers_광물캐기.js
@@ -1,37 +1,3 @@
-// function solution(picks, minerals) {
-//   var answer = Infinity;
-
-//   const mineralsOfPick = {
-//     diamond: [1, 5, 25],
-//     iron: [1, 1, 5],
-//     stone: [1, 1, 1],
-//   };
-
-//   const len = minerals.length;
-//   const totalToolCnt = picks.reduce((a, b) => a + b, 0);
-
-//   const search = (idx, tool, d, i, s, tired) => {
-//     const max = len < idx + 5 ? len : idx + 5;
-//     for (idx; idx < max; idx++) {
-//       tired += mineralsOfPick[minerals[idx]][tool];
-//     }
-
-//     if (len <= idx + 1 || d + i + s >= totalToolCnt) {
-//       answer = Math.min(answer, tired);
-//       return;
-//     }
-
-//     if (picks[0] > d) search(idx, 0, d + 1, i, s, tired);
-//     if (picks[1] > i) search(idx, 1, d, i + 1, s, tired);
-//     if (picks[2] > s) search(idx, 2, d, i, s + 1, tired);
-//   };
-
-//   if (picks[0]) search(0, 0, 1, 0, 0, 0);
-//   if (picks[1]) search(0, 1, 0, 1, 0, 0);
-//   if (picks[2]) search(0, 2, 0, 0, 1, 0);
-
-//   return answer;
-// }
 function solution(picks, minerals) {
   var answer = Infinity;
 
